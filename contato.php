38b939c41ba6de1fdb2510131c1eee039b768d7d
Babele-anna <144286237+Babele-anna@users.noreply.github.com>
2023-09-12 02:50:20 -0400

    Add files via upload

--- contato.php
+++ contato.php
@@ -1,32 +1,25 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Define o endereço de e-mail para onde você deseja enviar o formulário
-  $to = "lucianebatista@hotmail.com";
-  
-  // Recupera os campos do formulário
-  $nome = $_POST["nome"];
-  $email = $_POST["email"];
-  $telefone = $_POST["telefone"];
-  $mensagem = $_POST["mensagem"];
-  
-  // Monta o corpo do e-mail
-  $assunto = "Mensagem de Contato do Site";
-  $corpo = "Nome: $nome\n";
-  $corpo .= "E-mail: $email\n";
-  $corpo .= "Telefone: $telefone\n";
-  $corpo .= "Mensagem:\n\n$mensagem";
-  
-  // Define o cabeçalho do e-mail
-  $headers = "From: $email\r\n";
-  
-  // Envia o e-mail
-  $enviado = mail($to, $assunto, $corpo, $headers);
-  
-  if ($enviado) {
-    echo '<script>alert("Formulário de Contato enviado! Em breve entraremos em contato.");</script>';
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $telefone = $_POST['telefone'];
+  $mensagem = $_POST['mensagem'];
+
+  $to = "lucianebatista@hotmail.com"; // Substitua pelo seu endereço de e-mail
+  $subject = "Novo contato de $nome";
+  $message = "Nome: $nome\n";
+  $message .= "Email: $email\n";
+  $message .= "Telefone: $telefone\n";
+  $message .= "Mensagem:\n$mensagem";
+
+  $headers = "From: $email";
+
+  if (mail($to, $subject, $message, $headers)) {
+    echo '<script>alert("Obrigado por entrar em contato. Em breve responderemos.");</script>';
+    echo '<script>window.location.href = "formularioenviado.html";</script>';
   } else {
     echo '<script>alert("Ocorreu um erro ao enviar o formulário. Por favor, tente novamente mais tarde.");</script>';
+    echo '<script>window.location.href = "contato.html";</script>';
   }
 }
 ?>
-
