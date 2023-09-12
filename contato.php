3d517e88036c8dec4aec6407ccadf9761478f3dd
Babele-anna <144286237+Babele-anna@users.noreply.github.com>
2023-09-12 01:54:54 -0400

    Add files via upload

--- contato.php
+++ contato.php
@@ -1,26 +1,34 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST["nome"];
-    $email = $_POST["email"];
-    $telefone = $_POST["telefone"];
-    $mensagem = $_POST["mensagem"];
-    
-    $destinatario = "lucianebatista@hotmail.com"; // Substitua pelo seu email
-    $assunto = "Formulário de Contato - $nome";
-    
-    $mensagem_email = "Nome: $nome\n";
-    $mensagem_email .= "Email: $email\n";
-    $mensagem_email .= "Telefone: $telefone\n";
-    $mensagem_email .= "Mensagem:\n$mensagem\n";
-    
-    // Você também pode configurar cabeçalhos de email adequados aqui
-    
-    if (mail($destinatario, $assunto, $mensagem_email)) {
-        // Email enviado com sucesso
-        echo "Formulário de contato enviado com sucesso!";
-    } else {
-        // Erro ao enviar o email
-        echo "Ocorreu um erro ao enviar o formulário de contato.";
-    }
+  // Define as variáveis com base nos valores do formulário
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $telefone = $_POST['telefone'];
+  $mensagem = $_POST['mensagem'];
+
+  // Configurações de email
+  $destinatario = "lucianebatista@hotmail.com"; // Substitua pelo seu endereço de email
+  $assunto = "Formulário de Contato de $nome"; // Assunto do email
+
+  // Monta o corpo do email
+  $corpo_email = "Nome: $nome\n";
+  $corpo_email .= "Email: $email\n";
+  $corpo_email .= "Telefone: $telefone\n";
+  $corpo_email .= "Mensagem:\n\n$mensagem";
+
+  // Cabeçalhos do email
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+
+  // Envie o email
+  $enviado = mail($destinatario, $assunto, $corpo_email, $headers);
+
+  if ($enviado) {
+    // Email enviado com sucesso
+    $enviado = true;
+  } else {
+    // Erro ao enviar o email
+    $enviado = false;
+  }
 }
 ?>
