42661f6c235160ba73192fb8fa6b84ab4dcfa731
Babele-anna <144286237+Babele-anna@users.noreply.github.com>
2023-09-18 01:40:48 -0400

    Add files via upload

---
+++ js/contato.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $telefone = $_POST['telefone'];
+  $mensagem = $_POST['mensagem'];
+
+  $to = "contatobabeleanna@gmail.com"; // Substitua pelo seu endereço de e-mail
+  $subject = "Novo contato de $nome";
+  $message = "Nome: $nome\n";
+  $message .= "Email: $email\n";
+  $message .= "Telefone: $telefone\n";
+  $message .= "Mensagem:\n$mensagem";
+
+  $headers = "From: $email";
+
+  if (mail($to, $subject, $message, $headers)) {
+    echo '<script>alert("Obrigado por entrar em contato. Em breve responderemos.");</script>';
+    echo '<script>window.location.href = "enviado.html";</script>';
+  } else {
+    echo '<script>alert("Ocorreu um erro ao enviar o formulário. Por favor, tente novamente mais tarde.");</script>';
+    echo '<script>window.location.href = "contato.html";</script>';
+  }
+}
+?>
